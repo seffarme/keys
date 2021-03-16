44ec99e97795bbfb7414d35465e9dd69bf56971d
vanella tachago <staellevanella.tachago@essca.eu>
2021-03-16 17:32:24 +0100

    finished

--- app/controllers/biens_controller.rb
+++ app/controllers/biens_controller.rb
@@ -6,4 +6,24 @@ class BiensController < ApplicationController
   def show
     @bien = Bien.find(params[:id])
   end
+
+	def new
+		@bien = Bien.new
+	end
+
+	def create
+		@bien = Bien.new(bien_params)
+		@user = current_user
+		@bien.user = @user
+		if @bien.save
+			redirect_to bien_path(@bien)
+		else
+			render :new
+		end
+	end
+
+	private
+	def bien_params
+		params.require(:bien).permit(:nom, :categorie, :adresse, :ville, :code_postal, :pays, :info_compl_adresse, :surface, :nb_pieces, :nb_sdb, :nb_etages,:num_etage,:annee_construction, :prix_acquisition, :date_acquisition, :frais_achat_notaire, :frais_achat_agence, :frais_achat_travaux, :frais_achat_autres)
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_03_16_113220) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
