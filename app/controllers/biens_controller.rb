ca722387e5fc16bae26892f26c6df301fadc0ef3
vanella tachago <staellevanella.tachago@essca.eu>
2021-03-16 15:31:21 +0100

    Flats

--- app/controllers/biens_controller.rb
+++ app/controllers/biens_controller.rb
@@ -1,2 +1,25 @@
 class BiensController < ApplicationController
+	def new
+		@bien = Bien.new
+	end
+
+	def create
+	  # @bien = Bien.find(params[:bien_id])
+		# @bien = Bien.new(params[:bien])
+    # @bien.save
+	# end
+
+    @bien = Bien.new(bien_params)
+    @bien.save
+		@bien.user = current_user
+
+    # no need for app/views/biens/create.html.erb
+    redirect_to bien_path(@bien)
+  end
+
+  private
+
+  def bien_params
+    params.require(:bien).permit(:nom, :categorie, :adresse, :ville, :code_postal, :pays, :info_compl_adresse, :surface, :nb_pieces, :nb_sdb, :nb_etages, :num_etage, :annee_construction, :prix_acquisition, :date_acquisition, :frais_achat_notaire, :frais_achat_agence, :frais_achat_travaux, :frais_achat_autres)
+  end
 end
