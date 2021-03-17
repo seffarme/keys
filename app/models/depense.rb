ddc30ba33fcba25486eb8c0da559c909f9ddc674
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-03-17 11:30:06 +0100

    changes 2

--- app/models/depense.rb
+++ app/models/depense.rb
@@ -1,4 +1,13 @@
 class Depense < ApplicationRecord
   belongs_to :bien
   belongs_to :frais_recurrent, optional: true
+
+  def self.in_interval(date_debut, date_fin)
+    sql_query = " \
+      depenses.date_paiement >= :date_debut \
+      AND depenses.date_paiement <= :date_fin
+      "
+    @depenses = self.select(:montant).where(sql_query, date_debut: date_debut, date_fin: date_fin)
+  end
+
 end
