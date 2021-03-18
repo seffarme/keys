7f3ef80faacf4af510ec10b56fcc592df06697a9
julie laloy <julie.laloy@gmail.com>
2021-03-18 11:31:16 +0100

    gmail param

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,10 @@ module Keys
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-    config.action_mailer.delivery_method     = :postmark
-    config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-    config.action_mailer.default_url_options = { host: "www.keyz.work" }
+    # config.load_defaults 6.0
+    # config.action_mailer.delivery_method     = :postmark
+    # config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
+    # config.action_mailer.default_url_options = { host: "www.keyz.work" }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
