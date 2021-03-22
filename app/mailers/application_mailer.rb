0a27d39b6fddb64f23b5ec589acdfd7d727b7928
julie laloy <julie.laloy@gmail.com>
2021-03-22 17:42:26 +0100

    emailcorrectif2

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '#{@user.nom} <julie.laloy@gmail.com>'
+  default from: 'votre propriétaire <julie.laloy@gmail.com>'
   layout 'mailer'
 end
