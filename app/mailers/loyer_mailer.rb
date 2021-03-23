e18d112f5344c77e6bf7c35e8e5feea0abb8e341
julie laloy <julie.laloy@gmail.com>
2021-03-23 10:44:08 +0100

    relanceCorrectif

--- app/mailers/loyer_mailer.rb
+++ app/mailers/loyer_mailer.rb
@@ -4,12 +4,12 @@ class LoyerMailer < ApplicationMailer
     @loyer = loyer
     @locataire = @loyer.locataire
     @bien = @loyer.bien
-    mail(to: @locataire.email, subject:  "votre quittance pour la #{@bien.categorie} de #{@bien.nom}")
+    mail(to: @locataire.email, subject:  "votre quittance : #{@bien.nom}")
   end
 
   def relance(bien)
     @bien = bien
     @locataire = @bien.locataires.first
-    mail(to: @locataire.email, subject:  "RELANCE - Avis d'échéance pour la #{@bien.categorie} de #{@bien.nom}")
+    mail(to: @locataire.email, subject:  "RELANCE - Avis d'échéance : #{@bien.nom}")
   end
 end
