a6e225b4ae8447f175fe35f913baf434f7985dac
Mehdi Seffar <mehdi.seffar@outlook.fr>
2021-03-29 09:10:30 +0200

    try

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,4 +66,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
   # p "je usi sla"
   # p config.action_mailer.smtp_settings
+
+  config.hosts << "keyzimmo.herokuapp.com"
 end
